f0a6686adb19f0296e2468575e83f456649b24be
Ronny KWON <ronnykwon@gmail.com>
2020-10-27 12:53:27 -0930

    esgdfgj

--- WpfGit/MainWindow.xaml.cs
+++ WpfGit/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace WpfGit
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xamlc
     /// </summary>
     public partial class MainWindow : Window
     {
